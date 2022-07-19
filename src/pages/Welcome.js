3379b4a52debf894d22bce0c4d49d0b6d9852ab6
Josana-Kla <josana.0205@gmail.com>
2022-07-19 14:51:25 -0300

    feat: created page welcome

---
+++ src/pages/Welcome.js
@@ -0,0 +1,9 @@
+export default function Welcome() {
+    return(
+        <div>
+            <img src="../../public/assets/img/logo.png" alt="logo" />
+            <h1>ZapRecall</h1>
+            <button>Iniciar Recall!</button>
+        </div>
+    )
+}
