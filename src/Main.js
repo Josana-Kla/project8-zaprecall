b07c1f1fe38cba8e9ee8ed2b96cb3fa4aa4741b7
Josana-Kla <josana.0205@gmail.com>
2022-07-19 15:12:08 -0300

    feat: created deck page

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,11 @@
+import Deck from "./pages/Deck";
 import Welcome from "./pages/Welcome";
 
 export default function Main() {
     return(
         <div>
             <Welcome />
-
+            <Deck />
         </div>
     )
 }
---
+++ src/pages/Deck.js
@@ -0,0 +1,24 @@
+export default function Deck() {
+    return(
+        <div>
+            <div> 
+                <header>
+                    <img src="../../public/assets/img/logo-pequeno.png" alt="logo" />
+                </header>
+                <div className="flashcards">
+                    <div className="flashcard">
+                        <h2>Pergunta 1</h2>
+                        <ion-icon name=""></ion-icon>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <h3>0/4 CONCLUÍDOS</h3>
+                    <p>testando outros</p>
+                </div>
+            </div>
+        </div>
+    )
+}
