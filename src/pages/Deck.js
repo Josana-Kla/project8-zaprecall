07cd3c4c7ed8683eceb8b6974b879d1db01c5897
Josana-Kla <josana.0205@gmail.com>
2022-07-22 16:26:32 -0300

    style: finished basic structure of css

--- src/pages/Deck.js
+++ src/pages/Deck.js
@@ -1,24 +1,28 @@
 export default function Deck() {
     return(
-        <div className="content flex-center">
-            <div> 
-                <header>
+        <div>
+            <div className="content flex-center"> 
+                <header className="flex-center">
                     <img src="../../public/assets/img/small-logo.png" alt="logo" />
                 </header>
-                <div className="flashcards">
-                    <div className="flashcard">
+                <div className="flashcards flex-center">
+                    <button className="flex-center">
                         <h2>Pergunta 1</h2>
                         <ion-icon name="play-outline"></ion-icon>
-                    </div>
+                    </button>
+                    <button className="flex-center">
+                        <h2>Pergunta 1</h2>
+                        <ion-icon name="play-outline"></ion-icon>
+                    </button>
                 </div>
             </div>
 
-            <div>
-                <div>
+            <footer className="flex-center">
+                <div className="content-bottom">
                     <h3>0/4 CONCLUÍDOS</h3>
                     <p>testando outros</p>
                 </div>
-            </div>
+            </footer>
         </div>
     )
 }
