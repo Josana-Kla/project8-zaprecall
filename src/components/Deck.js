f551b63ce5718038a62fbee760cf7bbed2bf463d
Josana-Kla <josana.0205@gmail.com>
2022-07-23 15:37:19 -0300

    feat: created flashcards questions and choose answers components

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,8 +1,45 @@
+import React from "react";
+
 export default function Deck(props) {
+    const [ question, setQuestion ] = React.useState(true);
+    const [ arrow, setArrow ] = React.useState(true);
+
     return (
-        <button className="flex-center">
-            <h2>Pergunta {props.questionNumber}</h2>
-            <img src={props.iconName} alt=""></img>
-        </button>
+        <>
+            { question ? (
+                <button onClick={() => setQuestion(!question)} className="flex-center">
+                    <h2>Pergunta {props.questionNumber}</h2>
+                    <img src={props.iconName} alt="" />
+                </button>
+            ) : (
+                <FlashcardQuestion>
+                    <>
+                        { arrow ? (
+                            <>
+                                <p>O que é JSX?fkjd Lorem ajdaaihdiahfiuehdiasdoi aufhiuahdoiad aodhaiuehfaojdioaohfiua iauhda</p>
+                                <img onClick={() => setArrow(!arrow)} src="../../assets/img/arrow.svg" alt="" />
+                            </>
+                        ) : (
+                            <>
+                                <p>Oyuiooiuytrdfkytdxcvbnkiuytfdda</p>
+                                <div className="button-answers">
+                                    <button>Não lembrei</button>
+                                    <button>Quase não lembrei</button>
+                                    <button>Zap!</button>
+                                </div>
+                            </>
+                        )}
+                    </>
+                </FlashcardQuestion>
+            )}
+        </>
+    )
+}
+
+function FlashcardQuestion(props) {
+    return (
+        <div className="question">
+            {props.children}
+        </div>
     )
 }
